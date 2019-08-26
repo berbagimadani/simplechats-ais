a5e81d3c52fcf3d03b5c34ab6ea7194e41e5f63e
Ade Iskandar <berbagimadani@gmail.com>
2019-08-26 17:28:48 +0700

    up

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express'); 
-const logger = require('morgan');
+const logger  = require('morgan');
+const helmet  = require('helmet');
+//const cors    = require('cors');
+
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv'); 
-var path = require('path');
-//var cookieParser = require('cookie-parser');
+const path = require('path'); 
 
 const ApiRoutes = require('./api');
 
@@ -14,9 +16,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(express.urlencoded({ extended: true }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(helmet());
 
 app.use('/api', ApiRoutes);
 
